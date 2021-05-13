7e30dca23184099f7a99c65e9a954fb63aef3c3b
Piyush <mehtapiyush81@gmail.com>
2021-05-14 04:27:20 +0530

    sort added to temp

--- src/app/temp/temp.component.ts
+++ src/app/temp/temp.component.ts
@@ -1,4 +1,7 @@
+import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { TempApiCallService } from '../temp-api-call.service';
 import { DataSET } from '../tempDataSet';
@@ -9,6 +12,8 @@ import { DataSET } from '../tempDataSet';
   styleUrls: ['./temp.component.scss'],
 })
 export class TempComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   ELEMENT_DATA: DataSET[];
   displayedColumns: string[] = ['temp', 'time'];
   dataSource = new MatTableDataSource<DataSET>(this.ELEMENT_DATA);
@@ -16,6 +21,11 @@ export class TempComponent implements OnInit {
 
   ngOnInit(): void {
     this.getDataCall();
+    this.dataSource.paginator = this.paginator;
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
   public getDataCall() {
     let resp = this.service.getDataDB();

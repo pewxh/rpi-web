1764e03fab2cf3e2031b5587ac81ec20cdc9cc92
Piyush <mehtapiyush81@gmail.com>
2021-05-14 01:09:37 +0530

    basic ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+// import { LayoutModule } from '@angular/cdk/layout';
+import { AngularMaterialModule } from './material/material.module';
+import { TempComponent } from './temp/temp.component';
+import { LedComponent } from './led/led.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavComponent, TempComponent, LedComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

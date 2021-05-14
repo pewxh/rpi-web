1147e92aa44f7f1f9c2b1ad42adf2a24e4a32c60
Piyush <mehtapiyush81@gmail.com>
2021-05-14 16:46:21 +0530

    gpio table with temp content

--- src/app/led/led.component.ts
+++ src/app/led/led.component.ts
@@ -1,4 +1,10 @@
+import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { TempApiCallService } from '../temp-api-call.service';
+import { DataSET } from '../tempDataSet';
 
 @Component({
   selector: 'app-led',
@@ -6,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./led.component.scss'],
 })
 export class LedComponent implements OnInit {
-  constructor() {}
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  ELEMENT_DATA: DataSET[] = [];
+  displayedColumns: string[] = ['temp', 'time'];
+  dataSource = new MatTableDataSource<DataSET>(this.ELEMENT_DATA);
+  constructor(private service: TempApiCallService) {}
 
-  panelOpenState = false;
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getDataCall();
+    this.dataSource.paginator = this.paginator;
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  public getDataCall() {
+    let resp = this.service.getDataDB();
+    resp.subscribe((temp) => (this.dataSource.data = temp as DataSET[]));
+  }
 }
